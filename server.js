4e9202a076c7f423cbfd61f358c8a1e309df5028
anandmaraiya <anand.maraiya@gmail.com>
2016-04-14 12:10:51 +0530

    gg

--- server.js
+++ server.js
@@ -119,15 +119,15 @@ var demoData = [{ // dummy data to display
 	//local function
 	
 	var fetchToRepo = function(loc , file , callback){ 
-					child_process.spawn( 'cp '+loc+'/'+file+'  '+wd+'/uploads/UserData/'+file );			
+					var fileTransfer = child_process.spawn( 'cp '+loc+'/'+file+'  '+wd+'/uploads/UserData/'+file );			
 					callback(wd+'/uploads/UserData/'+file);
-					});
+					};
 					
 	var fetchToRbin = function(loc , file , callback){ 
-					child_process.spawn( 'cp '+loc+'/'+file+'  '+Rloc+'/'+file);
+					var fileTransfer = child_process.spawn( 'cp '+loc+'/'+file+'  '+Rloc+'/'+file);
 					callback(Rloc+'/'+file);
 					};
-	/*
+	
 	var RenameInRbin = function(fnameB , fnameA , callback){ 
 					var fileTransfer = child_process.exec( 'mv '+Rloc+'/'+fnameB+'  '+Rloc+'/'+fnameA);
 					callback(Rloc+'/'+fnameA);
@@ -136,7 +136,7 @@ var demoData = [{ // dummy data to display
 					var fileTransfer = child_process.exec( 'mv  '+wd+'/uploads/UserData/'+fnameB+'   '+wd+'/uploads/UserData/'+fnameA);
 					callback(wd+'/uploads/UserData/'+fnameA);
 					};
-	*/
+	
 	app.get('/info' , function(req,res) {
 			res.writeHead(200, {'content-type':'text/html'});
 			res.write('<script> alert("' + Rloc + '");</script>');
