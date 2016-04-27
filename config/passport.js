961e86f7415e28568d6f19235dc5b9b2c9f57d78
anandmaraiya <anand.maraiya@gmail.com>
2016-04-28 03:57:05 +0530

    dd

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 var _ = require('lodash');
 var passport = require('passport');
-var request = require('request');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/User');
 
